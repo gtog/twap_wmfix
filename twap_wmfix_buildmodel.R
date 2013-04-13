ca9479ab5d7b5750fadeed8418a13c449fb8d59c
Adam C. Duncan <aduncaninmotion@gmail.com>
2013-04-12 21:06:10 -0400

    initial commit
    Established R project, workspace, get_data, and buildmodel files.
    All of the data is present and the model is skeleton fitted.

---
+++ twap_wmfix_buildmodel.R
@@ -0,0 +1,115 @@
+# # twap_wmfix_buildmodel
+# Adam Duncan, Arnav Sheth Mar 2013
+
+# This section of the project builds the actual model we are trying to estimate.
+# There are a number of ways to do this. We can specify the model directly with calls
+# to glm or we can use the quantmod package and build the model with calls to specifyModel
+# and related functions. The latter has merits and well suited for the data we're using.
+# We can implement it both ways, but I'm going to start with the quantmod implementation.
+
+# begin:
+
+# Let's start by getting the FX into xts/zoo format. We can use the chron package to make
+# time based objects our to the strings contained in the time_stamps variable that comes from
+# the histdata.com data.
+
+# Function defs should go here:
+makeTimeStamps<-function(rawts) {
+  # Takes raw time stamp data from histdata.com downloads and converts it into a POSIXct format
+  # that we can pass to xts or other time based functions.
+  dates<-apply(as.matrix(rawts),MARGIN=2,FUN=substr,1,8) # character vector of extracted dates
+  times<-apply(as.matrix(rawts),MARGIN=2,FUN=substr,10,15) # character vector of extracted times
+  
+  # Add hypen to dates...
+  year<-substr(dates,1,4)
+  month<-substr(dates,5,6)
+  day<-substr(dates,7,8)
+  hour<-substr(times,1,2)
+  min<-substr(times,3,4)
+  sec<-substr(times,5,6)
+  newstamps<-paste(year,"-",month,"-",day," ",hour,":",min,":",sec,sep="")
+  ret<-strptime(newstamps,"%Y-%m-%d %H:%M:%S")
+  return(ret)
+}
+makeRHS<-function()
+# We need to get our variables in the right format before we proceed. This basically means we need
+# to convert the FX data we downloaded into xts/zoo format. The first step in doing so is creating 
+# a series of time stamps that are in a legitimate time based class supported by xts().
+
+fx.time.stamps<-makeTimeStamps(eurusd.data$time_stamp) # This can now be used to make xts/zoo series...
+data<-Cl(eurusd.data) # Op()=open, Hi()=high, Lo()=low, Cl()=close...
+
+fx.allt<-xts(data,order.by=fx.time.stamps) # All of the FX data for this pair...
+names(fx.allt)<-"fxrate"
+
+# Before we go further and assemble the dependent variables, we need to do a little more massaging 
+# of the independent variable. In this study, we are interested in a very particular time slice of 
+# data. Specifically, we are examining behavior in the FX market in the 1 hour interval between
+# 10 am - 11am NYT (or 3pm-4pm London). It is during this period that the majority of "fixing" business
+# happens. More detail will be provided later about how the 11am WMR fix works and why people use it.
+# Specifically, we want our independent variable to be the difference between the FX rate at exactly 11am
+# and the time-weighted average FX rate from 10am to 11am (the TWAP). That shouldn't be too tough, given
+# that we have converted the LHS variable to a time based class.
+
+fix.prices<-fx.allt[index(fx.allt,0)$hour==11 & index(fx.allt,0)$min==0] # A vector of all the 11am times. Our proxy for the WMR Fix.
+hour10to11<-fx.allt[(index(fx.allt,0)$hour>=10 & index(fx.allt,0)$hour<11)] # A vector of all the prices from 10am - 11am NYT.
+tempxts<-xts(as.vector(hour10to11),as.Date(index(hour10to11,0)))
+twap10to11<-suppressWarnings(xts(as.vector(by(tempxts,index(tempxts,0),mean)),index(fix.prices,0))) #THe TWAP from 10 to 11am NYT.
+names(twap10to11)<-"twap"
+rm(tempxts)
+
+# We have now successfully generated a once per day time series of 11am observations (fix.prices). This is
+# our proxy for the WMR fixing rate published every day at 11 am. We could get more nuanced about it, but that
+# will serve for now and any differences are unlikely to be meaningful. 
+# We have now also generated a time series of once per day time weighted average prices (twap10to11). THis series
+# is the average of the 60, 1 minute interval observations from 10am to 11am NYT. This is our TWAP measure.
+# We can now create our independent variable which is going to simply bet the difference between these two variables:
+# independet variable = (fix.prices - twap10to11).
+
+LHS<-xts((fix.prices-twap10to11),as.Date(index(fix.prices,0)))
+lhs.h<-xts(rep(0,length(LHS)),index(LHS,0))
+lhs.l<-xts(rep(0,length(LHS)),index(LHS,0))
+lhs.c<-xts(rep(0,length(LHS)),index(LHS,0))
+lhs.v<-xts(rep(0,length(LHS)),index(LHS,0))
+lhs.a<-xts(rep(0,length(LHS)),index(LHS,0))
+LHS<-merge.xts(LHS,lhs.h,lhs.l,lhs.c,lhs.v,lhs.a)
+LHS<-as.quantmod.OHLC(LHS,
+                 col.names = c("Open", "High",
+                               "Low", "Close",
+                               "Volume", "Adjusted"),
+                 name = NULL)
+
+
+
+# Specify the formula for our model. We might want to make a function to build this on the fly and allow for
+# easier edits/permutations.
+form<-as.formula(Op(LHS)~Cl(MERV)+Cl(BVSP)+Cl(GSPTSE)) #+IPC+GSPC+DOW+AORD+SSEC+HSI+BSESN+JKSE+KLSE+N225+NZ50+STI+KS11+TWII+ATX+BFX+FCHI+GDAXI+AEX+
+                   #+SSMI+FTSE+GD.AT+TA100+DJC+SEAX+OMXSPI+MCX
+
+# This is where I am going to depart from the traditional build using glm(...) and am going to 
+# use the specifyModel function from quantmod. I think this could give added flexibility later on.
+# We'll do it both ways in case the quantmod method doesn't pan out. See 'quantmod::specifyModel()' or
+# type '?specifyModel' for details on the function. We will also use Next() and Lag() to test different
+# model specifications. See also: getModelData,getSymbols, buildModel,tradeModel,formula setSymbolLookup.
+
+the_model<-specifyModel(form,na.rm=TRUE)
+the_model.build<-buildModel(the_model,method='lm',training.per=c('2012-01-02','2012-03-23'))
+fittedModel(the_model.build)
+plot(the_model.build)
+coef(the_model.build)
+plot(residuals(the_model.build),type="l")
+fit<-fitted.values(the_model.build)
+plot(fit,type="l",main="Fitted Values")
+
+# Comment: Ok. This is a good setup so far. From here, we can refine the specification of the model. (the 'form" variable)
+# TO DO:
+# 1. exand 'form' variable to include all the indices.
+# 2. convert RHS variables from Cl(x) to OpCl() or take the first differences of the indices. We want to model 
+# CHANGES in the indices against the LHS variable.
+# 3. Add some helper functions to make it easier to tinker with the data.
+
+
+
+
+
+
---
+++ twap_wmfix_getdata.R
@@ -0,0 +1,162 @@
+# twap_wmfix_getdata
+# Adam Duncan, Arnav Sheth Mar 2013
+
+# This script will establish all of the variables and get the data into the workspace.
+# Adjust paths for local data as needed. (FX minute interval data)
+
+# begin:
+# Required Libraries: (I use the same base set regardless of use)
+
+suppressWarnings(library(zoo))
+suppressWarnings(library(xts))
+suppressWarnings(library(quantmod))
+suppressWarnings(library(PerformanceAnalytics))
+suppressWarnings(library(ggplot2))
+suppressWarnings(library(plyr))
+suppressWarnings(library(knitr))
+suppressWarnings(library(chron))
+
+# Establish data source and tickers for each of the indices. These will be our RHS variables.
+# Function getData() takes the tickers and the data source vectors and puts the variables 
+# into the workspace. Human labels are given using the labels.xxx variables.
+# makeIndex() is a function that returns an xts object that is index to 100 at the beginning
+# of the time series and evolves as the log returns of the input index. Useful for comparing
+# many indices of different base. 
+
+# Global parameters should be defined here:
+data.source = c("yahoo")
+first.date<-c("2012-01-02/") # Base date for the analysis. 
+year<-substr(first.date,1,4)
+OHLC.indices<-c("Close") # Closing index value for the DAY...(ie. each DAY has an OHLC)
+OHLC.FX("Close") # Closing bid FX price for the MINUTE...(ie. each MINUTE has an OHLC)
+
+# Function definitions should be defined here:
+getData<-function(tickers,datasrc){
+  for (i in 1:length(tickers)){
+    cat(tickers[i],i,"\n")
+    getSymbols(tickers[i],src=datasrc,
+               auto.assign=getOption("getSymbols.auto.assign",TRUE),
+               env=parent.frame())
+  }
+}
+makeIndex<-function(x,inv,ret){
+  # Takes an xts object x and returns an index starting at 100 and evolving as the log returns of x.
+  # The inv flag tells whether or not to invert the series before calculating returns.
+  # The ret flag tells whether or not we have been passed a series of returns already.
+  init.val<-100
+  dts<-index(x,0)
+  if (inv==TRUE) data<-1/x else data<-x
+  if (ret==TRUE){ # we have a series of returns...
+    ret.series<-x
+  } else {
+    ret.series<-periodReturn(data,period="daily",subset=NULL,type="log")
+    dts<-index(ret.series,0)
+  }
+  n<-length(ret.series)
+  new.series<-ret.series
+  new.series[1]<-init.val
+  
+  for (i in 2:n){
+    new.series[i]<-(1+ret.series[i-1])*new.series[i-1]
+  }
+  names(new.series)<-c("index")
+  return(new.series)
+} # My custom index funtion for converting indices to 100 based at inception.
+getOHLC<-function(assets,OHLC){
+  # Takes a list of assets and returns either the Open, High, Low, or Close depending
+  # on the passed value of HLOC. Return value is of type xts/zoo.
+  ret<-NULL
+  for (i in 1:length(assets)){
+    if (OHLC=="O" || OHLC=="Open"){
+      ret<-cbind(ret,assets[[i]][,1])
+    } else {
+      if (OHLC=="H" || OHLC=="High"){
+        ret<-cbind(ret,assets[[i]][,2])
+      } else {
+        if (OHLC=="L" || OHLC=="Low"){
+          ret<-cbind(ret,assets[[i]][,3])
+        } else {
+          if (OHLC=="C" || OHLC=="Close"){
+            ret<-cbind(ret,assets[[i]][,4])
+          }
+        }
+      }
+    }
+  }
+  return(ret)
+}
+
+# Make symbol collection...
+tickers.americas=c("^MERV","^BVSP","^GSPTSE","^IPC","^GSPC","DOW")
+tickers.asiapac=c("^AORD","^SSEC","^HSI","^BSESN","^JKSE","^KLSE","^N225","^NZ50","^STI","^KS11","^TWII")
+tickers.europe=c("^ATX","^BFX","^FCHI","^GDAXI","^AEX","^OSEAX","^OMXSPI","^SSMI","^FTSE",
+                 "^MCX","GD.AT")
+tickers.africame=c("^TA100")
+tickers.commod=c("DJC")
+
+labels.americas=c("Merval","Bovespa","SP.TSX.Comp","MXX","SP500","Dow")
+labels.asiapac=c("All Ordinaries","Shanghai Comp","Hang Seng","BSE30","Jakarta Comp",
+                 "KLSE Comp","Nikkei 225","NZSE50 Index","Straits Times Index","KOSPI Index",
+                 "Taiwan Weighted")
+labels.europe=c("ATX Index","BEL20 Index","CAC40 Index","DAX Index","AEX General","OSE All Share",
+                "Stockholm General","Swiss Market","FTSE 100","MICEX Index","Athex Composite")
+labels.africame=c("TA 100")
+labesl.commod=c("DJ-UBS Comm. Index")
+
+# Go get the data...
+suppressWarnings(getData(tickers.americas,data.source))
+suppressWarnings(getData(tickers.asiapac,data.source))
+suppressWarnings(getData(tickers.europe,data.source))
+suppressWarnings(getData(tickers.africame,data.source))
+suppressWarnings(getData(tickers.commod,data.source))
+
+# Import some high frequency data from local files...(Year 2012 in 1 minute intervals. Can be exanded.)
+eurusd.data<-read.csv(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_EURUSD_M1_",year,".csv")) 
+usdjpy.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDJPY_M1_",year,".csv",sep=""), sep=";", quote="\"")
+gbpusd.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_GBPUSD_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdchf.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDCHF_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdnok.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDNOK_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdsek.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDSEK_M1_",year,".csv",sep=""), sep=";", quote="\"")
+audusd.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_AUDUSD_M1_",year,".csv",sep=""), sep=";", quote="\"")
+nzdusd.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_NZDUSD_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdcad.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDCAD_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdmxn.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDMXN_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdtry.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDTRY_M1_",year,".csv",sep=""), sep=";", quote="\"")
+usdzar.data<-read.table(paste("~/R/R Projects/twap_wmfix/data/",year,"/DAT_ASCII_USDZAR_M1_",year,".csv",sep=""), sep=";", quote="\"")
+
+# Establish common names for all the colums...
+names(usdjpy.data)<-names(eurusd.data)
+names(gbpusd.data)<-names(eurusd.dataf)
+names(usdchf.data)<-names(eurusd.dataf)
+names(usdnok.data)<-names(eurusd.dataf)
+names(usdsek.data)<-names(eurusd.dataf)
+names(audusd.data)<-names(eurusd.dataf)
+names(nzdusd.data)<-names(eurusd.dataf)
+names(usdcad.data)<-names(eurusd.dataf)
+names(usdmxn.data)<-names(eurusd.dataf)
+names(usdtry.data)<-names(eurusd.dataf)
+names(usdzar.data)<-names(eurusd.dataf)
+
+# Visually inspect a couple data elements to makes sure imports went well...
+head(IPC)
+head(KLSE)
+head(eurusd.data)
+head(gbpusd.data)
+
+# Create vectors for all of the indices we have collected. Each of the indices imported
+# from the data source will be of type 'zoo' / 'xts'. That's good because they are time based 
+# objects. We'll want to make sure our minute interval FX data is also of the same class.
+
+indices.americas<-c(MERV[first.date],BVSP[first.date],GSPTSE[first.date],IPC[first.date],GSPC[first.date],DOW[first.date])
+inices.asiapac<-c(AORD[first.date],SSEC[first.date],HSI[first.date],BSESN[first.date],JKSE[first.date],KLSE[first.date],
+                   N225[first.date],NZ50[first.date],STI[first.date],KS11[first.date],TWII[first.date])
+indices.europe<-c(ATX[first.date],BFX[first.date],FCHI[first.date],GDAXI[first.date],AEX[first.date],
+                  OSEAX[first.date],OMXSPI[first.date],SSMI[first.date],FTSE[first.date],
+                    MCX[first.date],GD.AT[first.date])
+indices.africame<-c(TA100[first.date])
+indices.commod<-c(DJC[first.date])
+
+# end twap_wmfix_getdata
+
+
+
